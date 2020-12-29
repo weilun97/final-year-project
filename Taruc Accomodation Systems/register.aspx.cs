1a4db5753e15449fa430fbd1e6b472d4863a1e53
Wei Lun <Wei Lun@DESKTOP-KQPC2PT>
2020-12-29 15:34:47 +0800

    build connection with database.mdf

--- Taruc Accomodation Systems/register.aspx.cs
+++ Taruc Accomodation Systems/register.aspx.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace Taruc_Accomodation_Systems
 {
+
     public partial class register : System.Web.UI.Page
     {
+        SqlConnection sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnregister_Click(object sender, EventArgs e)
         {
 
         }
---
+++ dbo.register.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[register]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [fname] VARCHAR(50) NULL, 
+    [lname] VARCHAR(50) NULL, 
+    [username] VARCHAR(50) NULL, 
+    [password] VARCHAR(50) NULL, 
+    [email] VARCHAR(50) NULL, 
+    [mname] VARCHAR(50) NULL
+)
